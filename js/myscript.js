e632f1c82e43509c80f2d2f8191ea8a03894a908
Fede <chiellofederico@gmail.com>
2021-01-24 17:26:03 +0100

    filter array

---
+++ js/myscript.js
@@ -0,0 +1,121 @@
+// (1) Partendo dall'array di oggetti icons, aggiungere i colori ad ogni categoria: blue per gli animali, orange per i vegetali e viola per gli utenti. Prendete i colori da un altro array.
+// (2) Stampare poi tutte le icone utilizzando il template literal.
+// (3) Stampare quindi nella select tutti i tipi che avete in precedenza selezionato (animal, vegetable, icon).
+// (4) Filtrare i risultati in base alla categoria (ricordate di svuotare il container).
+// Utilizzate forEach, map e filter e cercate di strutturare tutto con le funzioni.
+
+$(document).ready(function(){
+
+  // Creiamo un array di oggetti di icone.
+  const icone = [
+    {
+      name: 'cat',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'crow',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dog',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dove',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dragon',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'horse',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'hippo',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'fish',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'carrot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'apple-alt',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'lemon',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'pepper-hot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'user-astronaut',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-graduate',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-ninja',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-secret',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    }
+  ];
+
+  // Utilizzo il filter per creare tre nuovi array divisi per tipo: gli animali, le verdure e gli utenti.
+  const gruppoAnimali = icone.filter((element) => element.type == 'animal');
+  console.log(gruppoAnimali);
+  const gruppoVerdure = icone.filter((element) => element.type == 'vegetable');
+  console.log(gruppoVerdure);
+  const gruppoUtenti = icone.filter((element) => element.type == 'user');
+  console.log(gruppoUtenti);
+
+
+
+
+
+});
