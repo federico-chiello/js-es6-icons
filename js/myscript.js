58cbc07c53dd6c3fbf2e12a1b0a36f2a3e218160
Fede <chiellofederico@gmail.com>
2021-01-24 18:36:34 +0100

    Inserisco gli altri tipi nel select

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,12 @@
-// (1) Partendo dall'array di oggetti icons, aggiungere i colori ad ogni categoria: blue per gli animali, orange per i vegetali e viola per gli utenti. Prendete i colori da un altro array.
-// (2) Stampare poi tutte le icone utilizzando il template literal.
-// (3) Stampare quindi nella select tutti i tipi che avete in precedenza selezionato (animal, vegetable, icon).
-// (4) Filtrare i risultati in base alla categoria (ricordate di svuotare il container).
+// Partendo dall'array di oggetti icons, aggiungere i colori ad ogni categoria: blue per gli animali, orange per i vegetali e viola per gli utenti. Prendete i colori da un altro array.
+// Stampare poi tutte le icone utilizzando il template literal.
+// Stampare quindi nella select tutti i tipi che avete in precedenza selezionato (animal, vegetable, icon).
+// Filtrare i risultati in base alla categoria (ricordate di svuotare il container).
 // Utilizzate forEach, map e filter e cercate di strutturare tutto con le funzioni.
 
 $(document).ready(function(){
 
-  // Creiamo un array di oggetti di icone.
+  // (1) Creiamo un array di oggetti di icone.
   const icone = [
     {
       name: 'cat',
@@ -106,7 +106,7 @@ $(document).ready(function(){
     }
   ];
 
-  // Utilizzo il filter per creare tre nuovi array divisi per tipo: gli animali, le verdure e gli utenti.
+  // (2) Utilizzo il filter per creare tre nuovi array divisi per tipo: gli animali, le verdure e gli utenti.
   const gruppoAnimali = icone.filter((element) => element.type == 'animal');
   console.log(gruppoAnimali);
   const gruppoVerdure = icone.filter((element) => element.type == 'vegetable');
@@ -114,18 +114,16 @@ $(document).ready(function(){
   const gruppoUtenti = icone.filter((element) => element.type == 'user');
   console.log(gruppoUtenti);
 
-  // Creo un nuovo array in cui inserisco i colori che poi si dovranno aggiungere ai tre gruppi.
+  // (3) Creo un nuovo array in cui inserisco i colori che poi si dovranno aggiungere ai tre gruppi.
   const color = ['blue', 'orange', 'purple'];
 
-  // Metto l'elemento colore ad ogni gruppo di array.
+  // (4) Metto l'elemento colore ad ogni gruppo di array.
   gruppoAnimali.forEach((item) => item.color = 'blue');
   gruppoVerdure.forEach((item) => item.color = 'orange');
   gruppoUtenti.forEach((item) => item.color = 'purple');
 
-
-
-// **** Metodo senza funzione ****
-// Utilizzo un forEach per poter mettere dopo .icons nell'html i vari box contenenti gli animali, le verdure e gli utenti colorati.
+  // **** Metodo senza funzione ****
+  // Utilizzo un forEach per poter mettere dopo .icons nell'html i vari box contenenti gli animali, le verdure e gli utenti colorati.
   // icone.forEach((item) => {
   //   const {name, family, prefix, type, color} = item;
   //   $('.icons').append(
@@ -138,9 +136,20 @@ $(document).ready(function(){
   //   );
   // });
 
-// ***** Metodo con la funzione  richiamata *****
-  const boxIcon = $('.icons');
-  colorBox(icone, boxIcon);
+  // ***** Metodo con la funzione  richiamata *****
+  const boxIcone = $('.icons');
+  colorBox(icone, boxIcone);
+
+  // (5) Inserisco le altre opzioni del select.
+  const select = $('#type');
+  // Faccio un nuovo array dove inserisco i tipi da filtrare.
+  const filtraTipo = ['Animal', 'Vegetable', 'User'];
+  filtraTipo.forEach((item) => {
+    select.append(`
+      <option value="${item}">${item}</option>
+      `)
+  });
+
 
 
 
@@ -153,6 +162,7 @@ $(document).ready(function(){
 
 
 // ***** Funzioni *****
+// funzione che permette di creare con il template literal i vari box colorati dei tre gruppi.
 function colorBox(array, div) {
   array.forEach((item) => {
     const {name, family, prefix, type, color} = item;
